e0a0ef3bbcb74c36740f9958f7634c39c7cdba41
1nikhilff <129648398+1nikhilff@users.noreply.github.com>
2025-12-18 13:52:51 +0530

    Update __init__.py

--- gfetchert/__init__.py
+++ gfetchert/__init__.py
@@ -1,7 +1,10 @@
+"""
+__init__.py
+-----------
+Main package entry for Gfetchert.
+"""
+
 from .core import get_rainfall
-from .geocode import get_coordinates
-from .file_ops import fetch_from_csv
+from .file_ops import fetch_from_file
 
-__all__ = ["get_rainfall", "get_coordinates", "fetch_from_csv"]
-__version__ = "0.1.1"
-
+__all__ = ["get_rainfall", "fetch_from_file"]
